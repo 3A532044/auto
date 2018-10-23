5ff26866e49312f6e4f61fa51fa6790d91249b6b
林宥丞 <a26913700@gmail.com>
2018-10-24 01:47:19 +0800

    HelloWorld

---
+++ src/Demo/HelloWorld.php
@@ -0,0 +1,16 @@
+<?php
+namespace Demo;
+
+class HelloWorld
+{
+    function __construct()
+    {
+        echo 'Hello World ~'.PHP_EOL;
+    }
+}
+/**
+ * Created by PhpStorm.
+ * User: Monster
+ * Date: 2018/10/24
+ * Time: 上午 01:45
+ */
