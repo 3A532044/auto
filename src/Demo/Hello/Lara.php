1061ffc2185c5d5af4f2f54d4125ded41371a767
林宥丞 <a26913700@gmail.com>
2018-10-24 01:50:35 +0800

    \Demo\Hello\Lara

---
+++ src/Demo/Hello/Lara.php
@@ -0,0 +1,16 @@
+<?php
+namespace Demo\Hello;
+
+class Lara
+{
+    function __construct()
+    {
+        echo 'Hello Lara~'.PHP_EOL;
+    }
+}
+/**
+ * Created by PhpStorm.
+ * User: Monster
+ * Date: 2018/10/24
+ * Time: 上午 01:48
+ */
