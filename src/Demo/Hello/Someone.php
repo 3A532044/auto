4eb2fe59e03e9aead5cfd22dc9d3832a5de6d1d9
林宥丞 <a26913700@gmail.com>
2018-10-24 01:54:11 +0800

    \Demo\Hello\Someone

---
+++ src/Demo/Hello/Someone.php
@@ -0,0 +1,16 @@
+<?php
+namespace Demo\Hello;
+
+class Someone
+{
+    function __construct($name)
+    {
+        echo 'Hello '.$name.PHP_EOL;
+    }
+}
+/**
+ * Created by PhpStorm.
+ * User: Monster
+ * Date: 2018/10/24
+ * Time: 上午 01:53
+ */
